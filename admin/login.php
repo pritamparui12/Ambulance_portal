803f5dbb313a2e22a511eb1c9f1410c6d54d8d4a
pritamparui12 <pritamparui5@gmail.com>
2024-12-12 13:46:23 +0530

    working on login page

--- admin/login.php
+++ admin/login.php
@@ -27,6 +27,7 @@ if(isset($_POST['login']))
 <!-- //bootstrap-css -->
 <!-- Custom CSS -->
 <link href="css/style.css" rel='stylesheet' type='text/css' />
+<link rel="stylesheet" href="css/login.css">
 <link href="css/style-responsive.css" rel="stylesheet"/>
 <!-- font CSS -->
 <link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
